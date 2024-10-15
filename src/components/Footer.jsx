f55e3f8f7c9399cdb155573bebb53d26727b083d
ShuttlerBad <dileepasudara@gmail.com>
2024-10-15 14:58:51 +0530

    footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import { SOCIAL_MEDIA_LINKS } from "../constants"
+
+
+const Footer = () => {
+  return (
+    <div className="mb-8 mt-20">
+
+        <div className="flex items-center justify-center gap-8">
+            {SOCIAL_MEDIA_LINKS.map((link, index) => (
+                <a key={index} href={link.href} target="_black" rel="noopener noreferrer">
+                    {link.icon}
+                </a>
+            ))}
+        </div>
+        <p className="mt-8 text-center tracking-tighter text-neutral-500">
+            &copy;compileTab. All Rights Reserved.
+        </p>
+    </div>
+  )
+}
+
+export default Footer
